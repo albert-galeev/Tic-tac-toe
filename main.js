1e5360acaa35955a822ee73a93caa53e358a5886
Albert <31998800+seuraltimez@users.noreply.github.com>
2018-08-25 09:12:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var checkboxes = document.getElementsByTagName('input');
+for(var i=0; i<checkboxes.length; i++) {
+    checkboxes[i].indeterminate = true;
+}
+$('button').click(function() {
+var checkboxes = document.getElementsByTagName('input');
+for(var i=0; i<checkboxes.length; i++) {
+    checkboxes[i].indeterminate = true;
+}
+});
